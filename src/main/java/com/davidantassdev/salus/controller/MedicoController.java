4e5be63480b31a26bebcb556cc89af249b7b10a1
Davi Dantas <davidantasdev@gmail.com>
2026-01-27 11:36:20 -0300

    feat(medicos): add pagination and sorting

--- src/main/java/com/davidantassdev/salus/controller/MedicoController.java
+++ src/main/java/com/davidantassdev/salus/controller/MedicoController.java
@@ -1,15 +1,16 @@
 package com.davidantassdev.salus.controller;
 
 import com.davidantassdev.salus.medico.DadosCadastroMedico;
+import com.davidantassdev.salus.medico.DadosListagemMedico;
 import com.davidantassdev.salus.medico.Medico;
 import com.davidantassdev.salus.medico.MedicoRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/doctors")
@@ -21,5 +22,10 @@ public class MedicoController {
     @Transactional
     public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
         medicoRepository.save(new Medico(dados));
+    }
+
+    @GetMapping
+    public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
+        return medicoRepository.findAll(paginacao).map(DadosListagemMedico::new);
     }
 }
