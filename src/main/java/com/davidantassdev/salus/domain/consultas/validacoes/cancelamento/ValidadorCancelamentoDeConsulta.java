db4c149e190d563169f310711c4e2967c54ca933
Davi Dantas <davidantasdev@gmail.com>
2026-02-19 09:51:03 -0300

    feat(consulta): add cancellation validation rules

---
+++ src/main/java/com/davidantassdev/salus/domain/consultas/validacoes/cancelamento/ValidadorCancelamentoDeConsulta.java
@@ -0,0 +1,7 @@
+package com.davidantassdev.salus.domain.consultas.validacoes.cancelamento;
+
+import com.davidantassdev.salus.domain.consultas.DadosCancelamentoConsulta;
+
+public interface ValidadorCancelamentoDeConsulta {
+    void validar(DadosCancelamentoConsulta dados);
+}
